924f6bd3a5076c5110d0d68829a5d2c283f226c4
Maximiliano Damian Perez Martin <maximiliano.pm@gmail.com>
2023-07-26 13:46:02 -0300

    Cambios y Mejoras Generales
    Cambios y Mejoras Generales

--- BackEnd-NETCORE-API/IT-Test/IT-Test/Constants/BeerConstant.cs
+++ BackEnd-NETCORE-API/IT-Test/IT-Test/Constants/BeerConstant.cs
@@ -7,11 +7,13 @@ namespace IT_Test.Constants
     {
         private const string URL = "https://api.punkapi.com/v2/beers";
 
+        /*
         public static List<Beer> beers = new List<Beer>()
         {
             new Beer() { id = 1, name = "Buzz", description = "A light, crisp and bitter IPA brewed with English and American hops. A small batch brewed only once."},
             new Beer() { id = 2, name = "Trashy Blonde", description = "A titillating, neurotic, peroxide punk of a Pale Ale. Combining attitude, style, substance, and a little bit of low self esteem for good measure."}
         };
+        */
 
         public static List<Beer> APIBeers()
         {
